c62c43c21220f2d2be65233d183431a66b794709
mpitts <mpitts7@gmail.com>
2016-11-05 18:58:30 -0500

    Completion of week 1 lectures
    Final OO example from week 1 lectures.

---
+++ object_oriented.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+# Simple class from the lecture that demonstrates object oriented Python.
+
+class PartyAnimal:
+    x = 0
+    name = ""
+
+    # Define the "Constuctor":
+    def __init__(self,nam):
+        self.name = nam
+        print "constructed"
+
+    def party(self):
+        self.x = self.x + 1
+        print self.name,"party count",self.x
+
+    # Define the "Destructor":
+    def __del__(self):
+        print self.name,"destructed, final party couny",self.x
+
+class FootballFan(PartyAnimal):
+    points = 0
+
+    def touchdown(self):
+        self.points = self.points + 7
+        self.party()
+        print self.name,"points",self.points
+
+s = PartyAnimal("Sally")
+
+s.party()
+
+j = FootballFan("Jim")
+
+j.party()
+j.touchdown()
