8311d024d52c9a77a122fe43fe6a0c504e0babb2
Matt Pitts <matt@COMPOZED-G2-777.local>
2017-06-11 10:51:59 -0500

    Initial changes
    Just cleaned up some personal idioms.

--- Week 4 - Many-to-Many Relationships in SQL/roster.py
+++ Week 4 - Many-to-Many Relationships in SQL/roster.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import json
 import sqlite3
 
@@ -11,19 +13,19 @@ DROP TABLE IF EXISTS Member;
 DROP TABLE IF EXISTS Course;
 
 CREATE TABLE User (
-    id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-    name   TEXT UNIQUE
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name TEXT UNIQUE
 );
 
 CREATE TABLE Course (
-    id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-    title  TEXT UNIQUE
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    title TEXT UNIQUE
 );
 
 CREATE TABLE Member (
-    user_id     INTEGER,
-    course_id   INTEGER,
-    role        INTEGER,
+    user_id INTEGER,
+    course_id INTEGER,
+    role INTEGER,
     PRIMARY KEY (user_id, course_id)
 )
 ''')
@@ -45,18 +47,18 @@ for entry in json_data:
 
     print name, title
 
-    cur.execute('''INSERT OR IGNORE INTO User (name) 
+    cur.execute('''INSERT OR IGNORE INTO User (name)
         VALUES ( ? )''', ( name, ) )
     cur.execute('SELECT id FROM User WHERE name = ? ', (name, ))
     user_id = cur.fetchone()[0]
 
-    cur.execute('''INSERT OR IGNORE INTO Course (title) 
+    cur.execute('''INSERT OR IGNORE INTO Course (title)
         VALUES ( ? )''', ( title, ) )
     cur.execute('SELECT id FROM Course WHERE title = ? ', (title, ))
     course_id = cur.fetchone()[0]
 
     cur.execute('''INSERT OR REPLACE INTO Member
-        (user_id, course_id) VALUES ( ?, ? )''', 
+        (user_id, course_id) VALUES ( ?, ? )''',
         ( user_id, course_id ) )
 
     conn.commit()
