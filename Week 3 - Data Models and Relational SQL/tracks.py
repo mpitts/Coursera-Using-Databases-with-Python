4cffbf836be1adc53a812aa6d6b19e4695f2ef97
Matt Pitts <matt@COMPOZED-G2-777.local>
2017-06-11 09:32:15 -0500

    Added Genre table
    Updated script to include the Genre table.

--- Week 3 - Data Models and Relational SQL/tracks.py
+++ Week 3 - Data Models and Relational SQL/tracks.py
@@ -9,26 +9,33 @@ cur = conn.cursor()
 # Make some fresh tables using executescript()
 cur.executescript('''
 DROP TABLE IF EXISTS Artist;
+DROP TABLE IF EXISTS Genre;
 DROP TABLE IF EXISTS Album;
 DROP TABLE IF EXISTS Track;
 
 CREATE TABLE Artist (
-    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-    name    TEXT UNIQUE
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name TEXT UNIQUE
+);
+
+CREATE TABLE Genre (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name TEXT UNIQUE
 );
 
 CREATE TABLE Album (
-    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-    artist_id  INTEGER,
-    title   TEXT UNIQUE
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    artist_id INTEGER,
+    title TEXT UNIQUE
 );
 
 CREATE TABLE Track (
-    id  INTEGER NOT NULL PRIMARY KEY
-        AUTOINCREMENT UNIQUE,
-    title TEXT  UNIQUE,
-    album_id  INTEGER,
-    len INTEGER, rating INTEGER, count INTEGER
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    title TEXT UNIQUE,
+    album_id INTEGER,
+    len INTEGER,
+    rating INTEGER,
+    count INTEGER
 );
 ''')
 
