de72148790409f289b44a1aaacaffad4677e01f4
Matt Pitts <matt@COMPOZED-G2-777.local>
2017-06-11 10:56:03 -0500

    Completed Week 4
    Completion of Week 4 assignment.

--- Week 4 - Many-to-Many Relationships in SQL/roster.py
+++ Week 4 - Many-to-Many Relationships in SQL/roster.py
@@ -44,21 +44,20 @@ for entry in json_data:
 
     name = entry[0];
     title = entry[1];
+    role = entry[2]
 
-    print name, title
+    print name, title, role
 
-    cur.execute('''INSERT OR IGNORE INTO User (name)
-        VALUES ( ? )''', ( name, ) )
+    cur.execute('INSERT OR IGNORE INTO User (name) VALUES ( ? )', ( name, ) )
     cur.execute('SELECT id FROM User WHERE name = ? ', (name, ))
     user_id = cur.fetchone()[0]
 
-    cur.execute('''INSERT OR IGNORE INTO Course (title)
-        VALUES ( ? )''', ( title, ) )
+    cur.execute('INSERT OR IGNORE INTO Course (title) VALUES ( ? )', ( title, ) )
     cur.execute('SELECT id FROM Course WHERE title = ? ', (title, ))
     course_id = cur.fetchone()[0]
 
     cur.execute('''INSERT OR REPLACE INTO Member
-        (user_id, course_id) VALUES ( ?, ? )''',
-        ( user_id, course_id ) )
+        (user_id, course_id, role) VALUES ( ?, ?, ? )''',
+        ( user_id, course_id, role ) )
 
     conn.commit()
